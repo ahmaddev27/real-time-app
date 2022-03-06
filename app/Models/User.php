c00be3d498ebba4fb59248f05a934c5bfe2fd1c3
A7madjaber <aa20589@gmail.com>
2022-03-06 23:43:48 +0200

    JWT Auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,17 +6,18 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use  HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+
     protected $fillable = [
         'name',
         'email',
@@ -44,6 +45,21 @@ class User extends Authenticatable
 
 
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     public function questions(){
         return $this->hasMany(Question::class);
     }
@@ -51,4 +67,6 @@ class User extends Authenticatable
     public function replies(){
         return $this->hasMany(Reply::class);
     }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,16 @@ Route::apiResource('{question}/reply',\App\Http\Controllers\Api\ReplyController:
 
 Route::post('{reply}/like/{user_id}',[\App\Http\Controllers\Api\LikeController::class,'like']);
 Route::delete('{reply}/like/{user_id}',[\App\Http\Controllers\Api\LikeController::class,'unlike']);
+
+
+
+
+Route::group(['middleware' => 'api'], function () {
+
+    Route::post('login',[\App\Http\Controllers\Api\AuthController::class,'login'])->name('login');
+    Route::post('register',[\App\Http\Controllers\Api\AuthController::class,'register']);
+    Route::post('logout',  [\App\Http\Controllers\Api\AuthController::class,'logout']);
+    Route::post('refresh',  [\App\Http\Controllers\Api\AuthController::class,'refresh']);
+    Route::post('profile', [\App\Http\Controllers\Api\AuthController::class,'userProfile']);
+
+});
